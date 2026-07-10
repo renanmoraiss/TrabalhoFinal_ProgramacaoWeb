18b083ac27bef3d373d657bac7eba858fa81f725
sabjoao <sabino.jp03@gmail.com>
2026-07-09 23:23:37 -0300

    feat/ adicionando funcao de remover usuario

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { AuthController } from "./Controller/Auth/AuthController";
 import { EditUserController } from "./Controller/User/EditUserController";
 import { isAuthenticated } from "./middleware/MeddlewareAuth";
 import { ListUserController } from "./Controller/User/ListUserController";
+import { DeleteUserController } from "./Controller/User/DeleteUserController";
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.post("/user/create", new CreateUserController().handle);
 router.post("/user/session", new AuthController().handle);
 router.put("/user/edit", isAuthenticated, new EditUserController().handle);
 router.get("/user/list", isAuthenticated, new ListUserController().handle);
+router.delete("/user/delete", isAuthenticated, new DeleteUserController().handle);
 
 
 export {router};
